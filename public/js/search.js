61caf7e4bd91d33aa02b568e0b14987ae18244af
Ana Maria <anamariatxt@gmail.com>
2025-05-12 23:16:16 +0100

    hide search bar function

--- public/js/search.js
+++ public/js/search.js
@@ -1,21 +1,21 @@
-searchInput.addEventListener("input", e => {
-    const value = e.target.value
-    console.log(value)
-})
+// searchInput.addEventListener("input", e => {
+//     const value = e.target.value
+//     console.log(value)
+// })
 
 
 
-document.addEventListener("DOMContentLoaded", () => {
-    const searchInput = document.getElementById("search");
-    const rows = document.querySelectorAll(".coin-row");
+// document.addEventListener("DOMContentLoaded", () => {
+//     const searchInput = document.getElementById("search");
+//     const rows = document.querySelectorAll(".coin-row");
 
-    searchInput.addEventListener("input", () => {
-            const query = searchInput.value.toLowerCase();
+//     searchInput.addEventListener("input", () => {
+//             const query = searchInput.value.toLowerCase();
 
-        rows.forEach(row => {
-            const tds = row.querySelectorAll('td');
-            const coinName = tds[1]?.textContent.toLowerCase() || "";
-            row.style.display = coinName.includes(query) ? "" : "none";
-        });
-    });
-});
+//         rows.forEach(row => {
+//             const tds = row.querySelectorAll('td');
+//             const coinName = tds[1]?.textContent.toLowerCase() || "";
+//             row.style.display = coinName.includes(query) ? "" : "none";
+//         });
+//     });
+// });
